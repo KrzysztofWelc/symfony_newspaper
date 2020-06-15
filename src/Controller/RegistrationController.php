130e9bb82c913d749d0a2cabdd2c0beeb47d7cc0
Krzysztof <krzysiekwelc18@gmail.com>
2020-06-15 14:45:12 +0200

    better registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -31,6 +31,7 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
+            $user->setRoles([User::ROLE_USER]);
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
